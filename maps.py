417b81c238b802bfb0ef9ac96471995173789b7c
Jeiran <jeiran@nyu.edu>
2015-01-14 00:14:16 -0500

    fixed problem w hexadecimal color codes

--- maps.py
+++ maps.py
@@ -1,3 +1,4 @@
+# Before running this, enter pygmaps folder and run: python setup.py build and python setup.py install
 import pygmaps
 import webbrowser
 import pandas as pd
@@ -8,7 +9,7 @@ mymap = pygmaps.maps(33.5, 43.5, 6.5)
 data = pd.read_csv('irq.csv')
 #data['number'] = range(1, len(data) + 1)
 data.date = pd.to_datetime(data.date)
-dcolor_dict = {'RED':'F47465', 'BLUE':'4EA8EC','GREEN':'78B678'}
+dcolor_dict = {'RED':'#F47465', 'BLUE':'#4EA8EC','GREEN':'#78B678'}
 
 data_goodLoc = data[pd.notnull(data.lat)]
 years = pd.DatetimeIndex(data_goodLoc['date']).year
@@ -16,24 +17,25 @@ months = pd.DatetimeIndex(data_goodLoc['date']).month
 groupYearMonth = data_goodLoc.groupby([years,months])
 aggregateYearMonth = groupYearMonth.agg({"log": len})
 
-# for year in years:
-#     for month in months:
-#         for ind in groupYearMonth.get_group((year,month)).index.values:
-#             lat = data.ix[ind].lat
-#             lon = data.ix[ind].lon
-#             color = dcolor_dict[data.ix[ind].dcolor]
-#             mymap.addradpoint(lat,lon, 1000, color)
-#         mymap.draw('mymap'+year+'-'+month+'.draw.html') 
-#         url = 'mymap'+year+'-'+month+'.draw.html'
-# webbrowser.open_new_tab(url) 
-
-year=2004
-month=1
-for ind in groupYearMonth.get_group((year,month)).index.values:
-    lat = data.ix[ind].lat
-    lon = data.ix[ind].lon
-    color = dcolor_dict[data.ix[ind].dcolor]
-    mymap.addradpoint(lat,lon, 1000, color)
-mymap.draw('mymap'+str(year)+'-'+str(month)+'.draw.html') 
-url = 'mymap'+str(year)+'-'+str(month)+'.draw.html'
+for year in years:
+    print "year is ",str(year)
+    for month in months:
+        for ind in groupYearMonth.get_group((year,month)).index.values:
+            lat = data.ix[ind].lat
+            lon = data.ix[ind].lon
+            color = dcolor_dict[data.ix[ind].dcolor]
+            mymap.addradpoint(lat,lon, 1000, color)
+        mymap.draw('mymap'+str(year)+'-'+str(month)+'.draw.html') 
+        url = 'mymap'+str(year)+'-'+str(month)+'.draw.html'
 webbrowser.open_new_tab(url) 
+
+# year=2004
+# month=1
+# for ind in groupYearMonth.get_group((year,month)).index.values:
+#     lat = data.ix[ind].lat
+#     lon = data.ix[ind].lon
+#     color = dcolor_dict[data.ix[ind].dcolor]
+#     mymap.addradpoint(lat,lon, 2000, color)
+# mymap.draw('mymap'+str(year)+'-'+str(month)+'.draw.html') 
+# url = 'mymap'+str(year)+'-'+str(month)+'.draw.html'
+# webbrowser.open_new_tab(url) 
